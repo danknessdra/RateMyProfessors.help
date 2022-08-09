7eccbdd9215e8586efec47ee2391d448e4b64eb2
danknessdra <danknessdra@gmail.com>
2022-08-08 18:55:00 -0700

    nothing

--- go/webserver.go
+++ go/webserver.go
@@ -1,27 +1,8 @@
 package main
-<<<<<<< HEAD
 import (
     "fmt"
-    "log"
     "net/http"
 )
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
-	log.Print("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    
 }
-=======
-
-import (
-	"net/http"
-	"fmt"
-)
-
-func main() {
-}
->>>>>>> 83695b6fa284c9f008a6a9f875b144175f5c9d07
