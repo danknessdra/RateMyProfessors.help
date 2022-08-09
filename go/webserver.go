97956f781e12caeb19d5dc7e03acb2fc479a0d25
yuuwe-n <duy.yuuwe@pm.me>
2022-08-08 18:48:21 -0700

    add

--- go/webserver.go
+++ go/webserver.go
@@ -27,21 +27,14 @@ for i in range(0,6):
 	professors = ["jeff", "saul", "white", "jesse", "raymond", "bhat"]
 	rating = [3.2, 3.4, 4.6, 4.2, 5, 0.0]
 	
-	print("\{Course : \"{0}\", Professor : \"{1}\", Rating : \"{2}\"".format(courses[i], professors[i], rating[i]))
+	print("\t{{Course : \"{0}\", Professor : \"{1}\", Rating : \"{2}\"}}".format(courses[i], professors[i], rating[i]))
 }
 
 /*
-for i in range(0,3):
-	courses = ["Math", "Physics", "English", "Chemistry", "History", "PE" ]
-	professors = ["jeff", "saul", "white"]
-	rating = [5.2, 3.4, 5.6]
-	
-	print("{0}, {1},{2}".format(courses[i], professors[i], rating[i]))
-
 for i in range(0,6):
 	courses = ["Math", "Physics", "English", "Chemistry", "History", "PE" ]
 	professors = ["jeff", "saul", "white", "jesse", "raymond", "bhat"]
 	rating = [3.2, 3.4, 4.6, 4.2, 5, 0.0]
 	
-	print("Course : \"{0}\", Professor : \"{1}\", Rating : \"{2}\"".format(courses[i], professors[i], rating[i]))
+	print("\t{{Course : \"{0}\", Professor : \"{1}\", Rating : \"{2}\"}}".format(courses[i], professors[i], rating[i]))
 */
