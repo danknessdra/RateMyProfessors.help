caaa588ffa32802bc7034997e85f86c7ddfbdda8
yuuwe-n <duy.yuuwe@pm.me>
2022-08-10 20:06:08 -0700

    parse

--- go/webserver.go
+++ go/webserver.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -17,6 +21,51 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func main() {
+	jsonFile, err := os.Open("../data/courses.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// User struct which contains a name
+	// a type and a list of social links
+	type Course struct {
+		Course     string  `json:"course"`
+		Department string  `json:"department"`
+		Prof       string  `json:"prof"`
+		Rating     float64 `json:"rating"`
+	}
+
+	type Courses struct {
+		Courses []Course `json:"courses"`
+	}
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our Users array
+	var courses Courses
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &courses)
+
+	// we iterate through every user within our users array and
+	// print out the user Type, their name, and their facebook url
+	// as just an example
+
+	count := 0
+
+	for i := 0; i < len(courses.Courses); i++ {
+		count += 1
+		fmt.Println(count)
+		fmt.Println("Department: " + courses.Courses[i].Department)
+		fmt.Println("Course: " + courses.Courses[i].Course)
+		fmt.Println("Professor: " + courses.Courses[i].Prof)
+
+		// kinda/kinda dont fully understand format float, but hopefully it means no exp, and truncate to only 1 place
+		fmt.Println("Rating: " + strconv.FormatFloat(courses.Courses[i].Rating, 'f', 1, 32))
+	}
+
 	router := httprouter.New()
 	// Index needs to be a handler, idk if ServeFile is a handler
 	router.GET("/", Index)
