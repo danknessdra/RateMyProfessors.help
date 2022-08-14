17fb23121edcda1524b62a091179b3784e87c748
danknessdra <danknessdra@gmail.com>
2022-08-14 13:09:31 -0700

    finish

--- go/js/script.js
+++ go/js/script.js
@@ -104,7 +104,7 @@ const Footer = () => {
     <div className="black footer1">
       <div className="content has-text-centered">
         <p>
-          ratemyprofessors.help by Duy Nguyen and Raymond Lin. This source code is licensed under the
+          ratemyprofessors.help by Duy Nguyen and Raymond Lin. The source code is licensed under the
            MIT license.
         </p>
       </div>
