486eaec7afc9d41d27a559304be9b78dd2459f48
danknessdra <danknessdra@gmail.com>
2022-08-14 21:16:30 -0700

    prepped for prod

--- go/js/script.js
+++ go/js/script.js
@@ -1,52 +1,59 @@
-
 const Banner = () => {
-    return (  
-    <section className="hero is-large-touch has-text-centered black has-text-white round">
-    <div className="hero-body">
-      <p className="title is-4 white">ratemyprofessors.help</p>
-      <p className="subtitle is-5 white">
-        A Rate My Professors Helper! beta
-      </p>
-    </div>
-    </section>);
+  return /*#__PURE__*/React.createElement("section", {
+    className: "hero is-large-touch has-text-centered black has-text-white round"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "hero-body"
+  }, /*#__PURE__*/React.createElement("p", {
+    className: "title is-4 white"
+  }, "ratemyprofessors.help"), /*#__PURE__*/React.createElement("p", {
+    className: "subtitle is-5 white"
+  }, "A Rate My Professors Helper! beta")));
 };
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
+    this.state = {
+      value: ''
+    };
     this.handleSchool = this.handleSchool.bind(this);
     this.handleCourse = this.handleCourse.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleSchool(event){
-    this.setState({school: event.target.value});
-  }
-  handleCourse(event){
-    this.setState({course: event.target.value});
+
+  handleSchool(event) {
+    this.setState({
+      school: event.target.value
+    });
   }
 
+  handleCourse(event) {
+    this.setState({
+      course: event.target.value
+    });
+  }
 
   handleSubmit(event) {
     let data = {
-        School: this.state.school,
-        Course: this.state.course,
+      School: this.state.school,
+      Course: this.state.course
     };
     fetch("/get_rank", {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(data)
-    }).then((response) => {
-        response.text().then(function (data) {
-            let result = JSON.parse(data);
-            console.log(result);
-            let title = result[0].course;
-            let htmlString = '';
-            for (let i = 0; i<result.length;++i) {
-                htmlString+= `<section class = "box">
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then(response => {
+      response.text().then(function (data) {
+        let result = JSON.parse(data);
+        // console.log(result);
+        let title = result[0].course;
+        let htmlString = '';
+
+        for (let i = 0; i < result.length; ++i) {
+          htmlString += `<section class = "box">
                 <div class="newResult">
                   <div class = "title is-5">${result[i].prof}</div>
                   <div>
@@ -55,68 +62,83 @@ class Form extends React.Component {
                   </div>
                 </div>
                 </div>
-                </section>`
-            }
-            Swal.fire({
-              title: "<strong>"+result[0].course+"<\strong>",
-              showClass: {
-                popup: 'animate__animated animate__fadeInDown'
-              },
-              hideClass: {
-                popup: 'animate__animated animate__fadeOutUp'
-              },
-              html: htmlString,
-              showCloseButton: true,
-              focusConfirm: false,
-              confirmButtonColor: '#5186ef',
-              confirmButtonText:
-                'Ok!',
-              confirmButtonAriaLabel: 'Thumbs up, great!',
-            })
+                </section>`;
+        }
+
+        Swal.fire({
+          title: "<strong>" + result[0].course + "<\strong>",
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          },
+          html: htmlString,
+          showCloseButton: true,
+          focusConfirm: false,
+          confirmButtonColor: '#5186ef',
+          confirmButtonText: 'Ok!',
+          confirmButtonAriaLabel: 'Thumbs up, great!'
         });
-    }).catch((error) => {
-        console.log(error)
+      });
+    }).catch(error => {
+      console.log(error);
     });
     event.preventDefault();
   }
 
   render() {
-    return( 
-      <section className="columns is-mobile is-centered">
-        <div className="column is-one-quarter-desktop is-one-half-tablet">
-    <form className = "box" onSubmit={this.handleSubmit}>
-       <select name="school" id="school" value={this.state.school} onChange={this.handleSchool}>
-        <option className="select-school" value="" disabled selected>Select School</option>
-        <option value="De Anza">De Anza</option>
-        {/* <option value="Berkeley">Berkeley</option> */}
-    </select><br></br>
-    <input type="subject" name="subject" id="subject"value={this.state.course} onChange={this.handleCourse} placeholder="Format: Course and ID I.E CS 46B" required>
-    </input><br></br>
-    <input type="submit" id="search" value="Search!"></input>
-    </form>
-      </div>
-      </section>
-    );
+    return /*#__PURE__*/React.createElement("section", {
+      className: "columns is-mobile is-centered"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "column is-one-quarter-desktop is-one-half-tablet"
+    }, /*#__PURE__*/React.createElement("form", {
+      className: "box",
+      onSubmit: this.handleSubmit
+    }, /*#__PURE__*/React.createElement("select", {
+      name: "school",
+      id: "school",
+      value: this.state.school,
+      onChange: this.handleSchool
+    }, /*#__PURE__*/React.createElement("option", {
+      className: "select-school",
+      value: "",
+      disabled: true,
+      selected: true
+    }, "Select School"), /*#__PURE__*/React.createElement("option", {
+      value: "De Anza"
+    }, "De Anza")), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      type: "subject",
+      name: "subject",
+      id: "subject",
+      value: this.state.course,
+      onChange: this.handleCourse,
+      placeholder: "Format: Course and ID I.E CS 46B",
+      required: true
+    }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      type: "submit",
+      id: "search",
+      value: "Search!"
+    }))));
   }
+
 }
+
 const Footer = () => {
-  return (  
-    <div className="black footer1">
-      <div className="content has-text-centered">
-        <p>
-          ratemyprofessors.help by Duy Nguyen and Raymond Lin. The source code is licensed under the
-           MIT license.
-        </p>
-      </div>
-    </div>);
-};
-// add container to root then render- updated for React 18
+  return /*#__PURE__*/React.createElement("div", {
+    className: "black footer1"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "content has-text-centered"
+  }, /*#__PURE__*/React.createElement("p", null, "ratemyprofessors.help by Duy Nguyen and Raymond Lin. The source code is licensed under the MIT license.")));
+}; // add container to root then render- updated for React 18
+
+
 const render1 = document.getElementById("home");
 const render2 = document.getElementById("form");
 const render3 = document.getElementById("footer");
 const root1 = ReactDOM.createRoot(render1);
 const root2 = ReactDOM.createRoot(render2);
 const root3 = ReactDOM.createRoot(render3);
-root1.render(<Banner />);
-root2.render(<Form />);
-root3.render(<Footer />);
+root1.render( /*#__PURE__*/React.createElement(Banner, null));
+root2.render( /*#__PURE__*/React.createElement(Form, null));
+root3.render( /*#__PURE__*/React.createElement(Footer, null));
